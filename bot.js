962967912e98e1e5bf098333ab98d4a913af3452
DRKXY ~ <121325177+Polarrnaruto@users.noreply.github.com>
2024-12-12 22:24:22 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,13 +21,13 @@ if(location.href.includes("https://meet.google.com")){
             let botCount=prompt("How many bots would you like to activate? *Enter in number format only* Recommended : less than 15");
             let link=prompt("Enter link bots should hijack; *Remember to use an email other than your school email!!*");
             if(botCount>15){
-                let makeSure=confirm("Bot Count ("+botCount+") is too high. it may crash your computer, are you sure you would like to continue?")
+                let makeSure=confirm("Bot Count ("+10+") is too high. it may crash your computer, are you sure you would like to continue?")
                 if(makeSure!==1){
                     alert("OK, Please try again.")
                 }
             }
             if(botCount<=15){
-            let check=confirm("You have asked for "+botCount+" Bots and you want to Hijack this meeting: "+link+" Click ok to confirm and start hijack!!");
+            let check=confirm("You have asked for "+10+" Bots and you want to Hijack this meeting: "+meet.google.com/qoa-hwrh-zcy+" Click ok to confirm and start hijack!!");
             if(check==1){
                 for(let i=0;i<botCount;i++){
                     window.open(link+"#BOT"+Number(i+1))
